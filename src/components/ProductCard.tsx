af9ec3d9dad82c12212517f54cbf40ec111e9a96
mohammed1215 <mohammedelbanawey264@gmail.com>
2026-06-18 18:14:51 +0300

    feat: Added Product Card for better readability and to enhance the rendering

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,27 @@
+import type { Product } from "../Hooks/useFetchData";
+import { StarRating } from "./StarRating";
+
+export const ProductCard = ({ product }: { product: Product }) => {
+    return (
+        <div className="border border-border p-3 rounded-lg">
+            <div className="mb-3 flex h-36 items-center justify-center rounded-xl bg-bg p-3">
+                <img
+                    src={product.image}
+                    alt={product.title}
+                    loading="lazy"
+                    className="h-full w-full object-contain"
+                />
+            </div>
+            <div>
+                <span className="text-muted">{product.category}</span>
+                <p className="text-ink">{product.title}</p>
+                <div className="mt-3">
+                    <StarRating
+                        rate={product.rating?.rate}
+                        count={product.rating?.count}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
